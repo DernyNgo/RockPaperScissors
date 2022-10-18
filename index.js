a5479de9fb4c9c230bb475cbf8ee3ae059b7753d
Danny Ngo <DannyGHNgo@gmail.com>
2022-10-18 02:27:33 -0700

    Added pseudocode to aid in preparation of code

---
+++ index.js
@@ -0,0 +1,12 @@
+// begin with a function called getComputerChoice that will randomly return either 'Rock', 'Paper', or 'Scissors'
+// Write a function that plays a single round of Rock Paper Scissors.
+// The function should take two parameters: playerSelection and computerSelection
+// Return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+// Make the function's playerSelection parameter case-insensitive so users can input Rock, ROCK, RoCK, etc...
+// you want to return, not log the results of the function call
+// Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end
+// ^ Use a loop 
+// Use console log to display the results of each round and the winner at the end
+// Use prompt() to get input from the user 
+
+
