a83bfc15656aa902508849dd879c65a3c0be8319
Danny Ngo <DannyGHNgo@gmail.com>
2022-10-19 22:17:36 -0700

    Reformatted code to be more legible

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // begin with a function called getComputerChoice that will randomly return either 'Rock', 'Paper', or 'Scissors'
-function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-        return choices[Math.floor(Math.random() * choices.length)];
-}
-
 // Write a function that plays a single round of Rock Paper Scissors.
 // The function should take two parameters: playerSelection and computerSelection.
 // Return a string that declares the winner of the round like so: "You Lose! Paper beats Rock."
+// Make the function's playerSelection parameter case-insensitive so useres can input Rock, ROCK, RoCK, etc.
+// Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end
+// Use console log to display the results of each round and the winner at the end
+// Use prompt() to get input from the user 
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+        return choices[Math.floor(Math.random() * choices.length)];
+}
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -23,24 +27,10 @@ function playRound(playerSelection, computerSelection) {
     
 }
 
-const playerSelection = prompt("Please enter either Rock, Paper, or Scissors!");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
-// Make the function's playerSelection parameter case-insensitive so useres can input Rock, ROCK, RoCK, etc.
-
-let choices = ['Rock', 'Paper', 'Scissors'];
-let choices2 = ['ROCK', 'PAPER', 'SCISSORS'];
-    console.log(choices.toLowerCase === choices2.toLowerCase);
-
-// Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end
-
-
-for (let i = 0; i < 5; i++) {
-    playRound();
+function game() {
+    for (let i = 0; i < 5; i++) {
+      let playerSelection = prompt("Please enter either Rock, Paper, or Scissors!").toLowerCase();
+      let computerSelection = getComputerChoice().toLowerCase();
+      console.log(playRound(playerSelection, computerSelection));
+    }
 }
-
-// Use console log to display the results of each round and the winner at the end
-
-// Use prompt() to get input from the user 
-
